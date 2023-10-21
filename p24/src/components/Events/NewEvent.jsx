631bdd9e2f415b59be29bc3028df479e6f0cd107
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-21 18:20:10 +0200

    L 378

--- p24/src/components/Events/NewEvent.jsx
+++ p24/src/components/Events/NewEvent.jsx
@@ -5,12 +5,17 @@ import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { createNewEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { queryClient } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
   });
 
   function handleSubmit(formData) {
@@ -32,7 +37,15 @@ export default function NewEvent() {
           </>
         )}
       </EventForm>
-      {isError && <ErrorBlock title="Failed to create event" message={error.info?.message || "Failed to create. Please check your inputs and try again later."}/>}
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event"
+          message={
+            error.info?.message ||
+            "Failed to create event. Please check your inputs and try again later."
+          }
+        />
+      )}
     </Modal>
   );
 }
