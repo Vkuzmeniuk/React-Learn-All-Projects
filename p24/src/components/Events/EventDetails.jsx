35c8c047172abe3c01db0aae2062c871dff9aa8d
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-22 13:59:20 +0200

    L 380

--- p24/src/components/Events/EventDetails.jsx
+++ p24/src/components/Events/EventDetails.jsx
@@ -16,7 +16,9 @@ export default function EventDetails() {
   const {mutate} = useMutation({
     mutationFn: deleteEvent,
     onSuccess: ()=> {
-      queryClient.invalidateQueries({queryKey: ['events']})
+      queryClient.invalidateQueries({queryKey: ['events'],
+      refetchType: 'none'
+    })
       navigate('/events');
     }
   });
