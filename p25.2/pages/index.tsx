a5b7dc9222b8d25fe332c870f6a7364294f1fcaf
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-24 23:32:38 +0200

    L 542

---
+++ p25.2/pages/index.tsx
@@ -0,0 +1,20 @@
+import MeetupList from '../components/meetups/MeetupList';
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?auto=format&fit=crop&q=80&w=1888&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        adress: 'Some address, 5, 12345 Some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        adress: 'Some address, 5, 12345 Some City',
+        description: 'This is a second meetup!'
+    }
+]
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS}/>;
+}
