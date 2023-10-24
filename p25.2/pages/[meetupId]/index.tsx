19e30919b2f493bdf2fa3ad9c472db72f0387203
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-25 00:04:53 +0200

    L 545

---
+++ p25.2/pages/[meetupId]/index.tsx
@@ -0,0 +1,11 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?auto=format&fit=crop&q=80&w=1888&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title="A first meetup"
+      address="Some street 5"
+      description="The meetup description"
+    />
+  );
+}
