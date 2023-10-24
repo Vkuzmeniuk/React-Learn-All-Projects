4a3436ca720781f7eb915878c0bc8ab07e2994d7
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-24 21:18:14 +0200

    L 540

--- p25/pages/news/index.tsx
+++ p25/pages/news/index.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">Next JS</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
