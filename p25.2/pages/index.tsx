49c060e84b4c772ed03094a30a11b8b9fffd5d11
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-10-26 00:08:02 +0200

    L 551

--- p25.2/pages/index.tsx
+++ p25.2/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -18,8 +19,15 @@ const DUMMY_MEETUPS = [
     description: "This is a second meetup!",
   },
 ];
-export default function HomePage() {
-  return (
-      <MeetupList meetups={DUMMY_MEETUPS} />
-  );
+export default function HomePage(props) {
+
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps(){
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  }
 }
